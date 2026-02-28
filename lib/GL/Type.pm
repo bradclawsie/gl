00a645ef4a02b688ff34857f71a8addc71f19cb5
Brad Clawsie <brad@b7j0c.org>
2026-02-28 13:52:41 -0600

    returns for GL::Crypt::JWT

--- lib/GL/Type.pm
+++ lib/GL/Type.pm
@@ -66,6 +66,13 @@ my $iv = 'Type::Tiny'->new(
 );
 __PACKAGE__->meta->add_type($iv);
 
+my $jwt = 'Type::Tiny'->new(
+  name       => 'JWT',
+  constraint => sub { $_ isa GL::Crypt::JWT },
+  message    => sub { 'bad jwt' },
+);
+__PACKAGE__->meta->add_type($jwt);
+
 my $key = 'Type::Tiny'->new(
   name       => 'Key',
   constraint => sub { m/^[\da-f]{$GL::Crypt::Key::LENGTH}$/x },
